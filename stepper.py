cbd2261d7a4cbf9389b74d2cec5574b069cb8279
Nick Driesen <156067639+Nick3sen@users.noreply.github.com>
2024-11-11 21:47:29 +0100

    stepper motor code

---
+++ stepper.py
@@ -0,0 +1,62 @@
+import RPi.GPIO as GPIO
+import time
+
+in1=11
+in2=12
+in3=13
+in4=15
+
+step_sleep = 0.002
+step_count = 4096
+direction = False #true is clockwise false is counter clockwise
+
+step_sequence = [[1,0,0,1], # find in documentation via github programmeren
+                 [1,0,0,0],
+                 [1,1,0,0],
+                 [0,1,0,0],
+                 [0,1,1,0],
+                 [0,0,1,0],
+                 [0,0,1,1],
+                 [0,0,0,1]]
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
+
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+GPIO.output(in3, GPIO.LOW)
+GPIO.output(in4, GPIO.LOW)
+motor_pins = [in1, in2, in3, in4]
+motor_step_counter = 0;
+
+def cleanup():
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
+    GPIO.cleanup()
+    
+try:
+    i = 0
+    for i in range(step_count):
+        for pin in range(0, len(motor_pins)):
+            GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
+        if direction==True:
+            motor_step_counter = (motor_step_counter - 1) % 8
+        elif direction==False:
+            motor_step_counter = (motor_step_counter + 1) % 8
+        else: # defensive programming
+            print( "uh oh... direction should *always* be either True or False" )
+            cleanup()
+            exit( 1 )
+        time.sleep( step_sleep )
+
+except KeyboardInterrupt:
+    cleanup()
+    exit( 1 )
+
+cleanup()
+exit( 0 )
