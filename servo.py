a942de8a8f332d0f5beb7e95758a0e31589ea226
Nick Driesen <156067639+Nick3sen@users.noreply.github.com>
2024-11-11 12:10:32 +0100

    main file added

---
+++ servo.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+from time import sleep
+#------------------------- Define variables
+servo_1 = 3
+#------------------------- Setup GPIO
+
+GPIO.setmode(GPIO.BOARD) #Pinmode set to board /pinout in terminal
+GPIO.setup(servo_1, GPIO.OUT) #Set pin on output
+p = GPIO.PWM(servo_1, 50) #pin + frequency
+p.start(0)
+
+#------------------------ calculate dutycycle to angle
+def angle(x):
+    if x > 180 or x < 0:
+        print("angle doesn't exist")
+        return
+    duty = x / 18 + 3
+    p.ChangeDutyCycle(duty)
+#------------------------ code
+while True:
+    angle(180)
+    sleep(1)
+    angle(0)
+    sleep(1)
