1d4dca199fe3d135e62db96b6da1c6f491ae5bf0
Nick Driesen <145384646+Ndriese@users.noreply.github.com>
2025-01-22 11:42:06 +0100

    Trying to get IK to work

---
+++ IK/test.py
@@ -0,0 +1,10 @@
+import ikpy.chain
+import numpy as np
+import ikpy.utils.plot as plot_utils
+
+my_chain = ikpy.chain.Chain.from_urdf_file("BITL.urdf")
+target_position = [ 0.1, -0.2, 0.1]
+print("The angles of each joints are : ", my_chain.inverse_kinematics(target_position))
+
+real_frame = my_chain.forward_kinematics(my_chain.inverse_kinematics(target_position))
+print("Computed position vector : %s, original position vector : %s" % (real_frame[:3, 3], target_position))
