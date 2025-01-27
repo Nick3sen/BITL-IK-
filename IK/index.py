56c3fcc6276df97b891e203474f1845d5b4ae847
Nick Driesen <145384646+Ndriese@users.noreply.github.com>
2025-01-27 15:39:49 +0100

    fixed plotting => removing widgets

--- IK/index.py
+++ IK/index.py
@@ -13,8 +13,8 @@ import os
 pwd_path= os.path.dirname(os.path.abspath(__file__))
 myfile = os.path.join(pwd_path, 'BITL.urdf')
 
-my_chain = ikpy.chain.Chain.from_urdf_file(myfile,active_links_mask=[False, True, True, True, True, True])
-target_position = [ 0, 0,0.46]
+my_chain = ikpy.chain.Chain.from_urdf_file(myfile,active_links_mask=[True, True, True, True, True, True])
+target_position = [ 0.1, 0.2,0.2]
 target_orientation = [-1, 0, 0]
 
 ik = my_chain.inverse_kinematics(target_position, target_orientation, orientation_mode="Y")
@@ -31,7 +31,6 @@ my_chain.plot(ik, ax, target=target_position)
 plt.xlim(-0.5, 0.5)
 plt.ylim(-0.5, 0.5)
 ax.set_zlim(0, 0.6)
-# plt.ion()
 plt.show()
 
 def doIK():
@@ -53,18 +52,17 @@ def move(x,y,z):
     target_position = [x,y,z]
     doIK()
     updatePlot()
-
-    # sendCommand(ik[1].item(),ik[2].item(),ik[3].item(),ik[4].item(),ik[5].item(),ik[6].item(),1)
+    sendCommand(ik[1].item(),ik[2].item(),ik[3].item(),ik[4].item(),ik[5].item(),ik[6].item(),1)
 
 move(0,0.2,0.3)
 
-# ser = serial.Serial('COM3',9600, timeout=1)
+ser = serial.Serial('COM3',9600, timeout=1)
 
-# def sendCommand(a,b,c,d,e,f,move_time):
-#     command = '0{:.2f} 1{:.2f} 2{:.2f} 3{:.2f} 4{:.2f} 5{:.2f} t{:.2f}\n'.format(math.degrees(a),math.degrees(b),math.degrees(c),math.degrees(d),math.degrees(e),math.degrees(f),move_time)
-#     ser.write(command.encode('ASCII'))
+def sendCommand(a,b,c,d,e,f,move_time):
+    command = '0{:.2f} 1{:.2f} 2{:.2f} 3{:.2f} 4{:.2f} 5{:.2f} t{:.2f}\n'.format(math.degrees(a),math.degrees(b),math.degrees(c),math.degrees(d),math.degrees(e),math.degrees(f),move_time)
+    ser.write(command.encode('ASCII'))
 
-# # we'll call sendCommand once with a move time of 4s so the robot slowly moves to the initial point
-# sendCommand(ik[1].item(),ik[2].item(),ik[3].item(),ik[4].item(),ik[5].item(),ik[6].item(),4)
+# we'll call sendCommand once with a move time of 4s so the robot slowly moves to the initial point
+sendCommand(ik[1].item(),ik[2].item(),ik[3].item(),ik[4].item(),ik[5].item(),ik[6].item(),4)
 
-# ser.close() 
+ser.close() 
